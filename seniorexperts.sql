e327fb48fa5a83c161e67c47a46103268be9984f
Gery <fsygeryy@gmail.com>
2024-06-01 11:09:23 +0700

    change structure table perusahaan (pass + id)

--- seniorexperts.sql
+++ seniorexperts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 31, 2024 at 06:18 AM
+-- Generation Time: Jun 01, 2024 at 04:08 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -59,9 +59,9 @@ CREATE TABLE IF NOT EXISTS `experts` (
 --
 
 INSERT INTO `experts` (`username_experts`, `nama_experts`, `alamat_experts`, `bidang_experts`, `CV_experts`, `password`) VALUES
-('ger', 'Gery Yulianto', 'sido', 'finance', '../experts_cv/Gery Yulianto CV.pdf', '81dc9bdb52d04dc20036dbd8313ed055'),
-('1', '1', '1', 'software', '../experts_cv/Gery Yulianto CV.pdf', 'c4ca4238a0b923820dcc509a6f75849b'),
-('2', '1', '1', 'software', '../experts_cv/2.pdf', 'c4ca4238a0b923820dcc509a6f75849b');
+('ger', 'Gery Yulianto', 'sido', 'Finance', '../experts_cv/Gery Yulianto CV.pdf', '81dc9bdb52d04dc20036dbd8313ed055'),
+('1', '1', '1', 'IT', '../experts_cv/Gery Yulianto CV.pdf', 'c4ca4238a0b923820dcc509a6f75849b'),
+('2', '1', '1', 'IT', '../experts_cv/2.pdf', 'c4ca4238a0b923820dcc509a6f75849b');
 
 -- --------------------------------------------------------
 
@@ -71,13 +71,13 @@ INSERT INTO `experts` (`username_experts`, `nama_experts`, `alamat_experts`, `bi
 
 DROP TABLE IF EXISTS `loker_perusahaan`;
 CREATE TABLE IF NOT EXISTS `loker_perusahaan` (
-  `nama_perusahaan` varchar(100) NOT NULL,
+  `id_perusahaan` int NOT NULL,
   `bidang_perusahaan` varchar(100) NOT NULL,
   `fee` int DEFAULT NULL,
   `Jenis` varchar(255) DEFAULT NULL,
   `lokasi` varchar(255) DEFAULT NULL,
   `jadwal` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`nama_perusahaan`,`bidang_perusahaan`)
+  PRIMARY KEY (`id_perusahaan`,`bidang_perusahaan`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -90,8 +90,17 @@ DROP TABLE IF EXISTS `perusahaan`;
 CREATE TABLE IF NOT EXISTS `perusahaan` (
   `nama_perusahaan` varchar(100) NOT NULL,
   `alamat_perusahaan` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`nama_perusahaan`)
+  `id_perusahaan` int NOT NULL,
+  `password_perusahaan` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_perusahaan`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `perusahaan`
+--
+
+INSERT INTO `perusahaan` (`nama_perusahaan`, `alamat_perusahaan`, `id_perusahaan`, `password_perusahaan`) VALUES
+('PT Test Nasional', 'Jalan Test Nasional', 111, '698d51a19d8a121ce581499d7b701668');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
